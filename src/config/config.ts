166b363b3e360504188abadf1753682e3955fba9
sumittriofi <sumitkumar.p@flyerssoft.com>
2024-03-26 00:33:59 +0530

    Fix issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export let config = {
   // mongodbURI:process.env.MONGO_DB_URI || "mongodb://localhost:27017/GenericApi",
   mongodbURI:
     process.env.MONGO_DB_URI ||
-    "mongodb+srv://sp348770:6hoHJyPci8ryZuO9@cluster0.4zkgcih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://sp348770:Z0qFxqZa1Nx5fl67@cluster00.m2v7wzq.mongodb.net/",
   port: process.env.PORT || 7050,
   dynamicModels: new Array(),
 };
